c29294190a8a4eea7bf3fd224a74f434fed960fb
lcarr7077 <lcarr7077@sohamcollege.org.uk>
2014-05-08 11:54:33 +0100

    Update greenBottles.py
    I improved the code with the help of Mrs Atkinson.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,7 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+num = 10
+while num >0:
+    print(num, " green bottles, hanging on the wall")
+    print(num, " green bottles, hanging on the wall")
+    num = num-1
+    print("And if 1 green bottle should acidentally fall,")
+    print("They'll be ",num, " green bottles hanging on the wall.")
